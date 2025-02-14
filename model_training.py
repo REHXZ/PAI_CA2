5fd4f00b8130bbc830a20ffffe0948b9b0c01266
y0ngjie <yongjie.23@ichat.sp.edu.sg>
2025-02-14 13:32:21 +0800

    - using online db instead

--- model_training.py
+++ model_training.py
@@ -18,6 +18,8 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+print(DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD)
+
 # Create a connection string
 connection_string = (
     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
